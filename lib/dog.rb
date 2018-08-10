cba81b71e16635f2c875526515ae2c9a94cd22e4
dami21 <dami.adenekan@schaumburgchristian.com>
2018-08-10 16:56:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,17 @@
 # dog.rb
 class Dog 
+  def name=(dog_name)
+    @name = dog_name
+  end
+  def name 
+    @name 
+  end 
   
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end 
+  def breed 
+    @breed
+  end 
 end 
+
